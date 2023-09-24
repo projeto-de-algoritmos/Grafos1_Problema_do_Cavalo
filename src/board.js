aec0cff8406937db75a200b1e1fc2056f4729384
brenob6 <brenob6@gmail.com>
2023-09-23 23:43:31 -0300

    add interface

--- src/board.js
+++ src/board.js
@@ -1,10 +1,24 @@
 create_board();
 
-function addEvent(element) {
+function squareEvent(element) {
 	element.addEventListener("click", () => {
 		const n = parseInt(element.getAttribute("id"));
-		knight_tour(n)
+		const arr = knight_tour(n);	
+		console.log(arr);
+		let count = 1;
+		for(let i in arr) {
+			const square = document.getElementById(arr[i]);
+			square.innerHTML = count;
+			count++;
+		}
 	})
+}
+
+function cleanBoard() {
+	let squares = document.getElementsByTagName("td");
+	for(let s in squares) {
+		squares[s].innerHTML = "x";
+	}
 }
 
 function create_board(){
@@ -16,7 +30,7 @@ function create_board(){
 		let row = document.createElement("tr");
 		for(let j = 0; j < 8; j++) {
 			col = document.createElement("td");	
-			addEvent(col);
+			squareEvent(col);
 			col.setAttribute("id", count);
 			if(color)
 				col.setAttribute("class", "white_square");
